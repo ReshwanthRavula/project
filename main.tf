fa993fa9f73b67bf7b6fc7e16fb2fba961c3cbb9
ReshwanthRavula <ravulareshwanth@gmail.com>
2024-02-28 22:55:23 -0500

    creating instance

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region = "us-west-2"  # Specify your desired AWS region
+    region = "us-west-1"  # Specify your desired AWS region
+    access_key = "AKIA3I6WI4JSVV5F3TGQ"
+    secret_key = "JxILkpMw+0mDH7jnv+HcU8nLED4jJXL0vVLpNWMn"
 }
+
+resource "aws_instance" "my_first_server" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  
+}
+
